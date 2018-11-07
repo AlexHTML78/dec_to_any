cbf41760a7540c9598cf0099aa03a32431c0864e
Alex <sashayushcovskiy@gmail.com>
2018-11-07 10:26:35 +0300

    Bug Fixes

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-"use strict";
 function decToAny() {
-  let dec = document.getElementById("decimal").value;
-  let arr = [];
-  const bin = document.getElementById("result");
-  const num_sys = document.getElementById("num_sys").value;
+  var dec = document.getElementById("decimal").value;
+  var arr = [];
+  var bin = document.getElementById("result");
+  var num_sys = document.getElementById("num_sys").value;
   while (dec > 0) {
     if (dec % num_sys > 9) {
       arr.push(String.fromCharCode(55 + (dec % num_sys)));
